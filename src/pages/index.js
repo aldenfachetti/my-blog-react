e7a7a562f5ce192f33b9693b977150e06f99d312
Alden Merlin <aldenmerlin@gmail.com>
2020-05-16 17:50:41 -0300

    Created About page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home"/>
     <h1>Home</h1>
+      <ul>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <Link to="/" activeStyle={{ color: 'red' }}>About (Gatsby Link)</Link>
+        </li>
+        <li>
+          <Link to="/about">About (Gatsby Link)</Link>
+        </li>
+      </ul>
   </Layout>
 )
 

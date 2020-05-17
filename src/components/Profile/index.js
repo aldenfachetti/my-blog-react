52f417adc06c45935a412972560017fbab62cd08
Alden Merlin <aldenmerlin@gmail.com>
2020-05-17 16:13:57 -0300

    Created Profile page

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="profile-wrapper">
+        <h1>Alden Merlin</h1>
+        <h2>Front-end Develor</h2>
+        <p> Membro da empresa Youse, gosto de tecnologia e desenho.</p>
+    </div>
+)
+
+export default Profile

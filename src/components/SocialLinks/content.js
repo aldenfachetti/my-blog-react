b6982750ee03cb23ee47e125afaad32db959b615
Alden Fachetti <alden.fachetti@youse.com.br>
2020-05-21 16:38:01 -0300

    Created elemtens in Profile Sidebar

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+const handle = "aldenmerlin"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Behance",
+    url: `https://www.behance.net/${handle}`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`,
+  },
+  {
+    label: "Pinterest",
+    url: `https://br.pinterest.com/${handle}`,
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/alden-fachetti",
+  },
+]
+
+export default links
